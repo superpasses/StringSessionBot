05bc9847b9bc7721842c43b905c624905a9a1a4d
superpasses <165167006+superpasses@users.noreply.github.com>
2024-07-04 05:42:17 +0530

    Update env.py

--- env.py
+++ env.py
@@ -6,7 +6,7 @@ load_dotenv()
 API_ID = os.getenv("API_ID", "20562331").strip()
 API_HASH = os.getenv("API_HASH", "9e3e4148e73756a85b95fc69980b678d").strip()
 BOT_TOKEN = os.getenv("BOT_TOKEN", "7264254030:AAGthiCzHc38jg1JdrTej7hy7yg3NNsUl9Y").strip()
-DATABASE_URL = os.getenv("DATABASE_URL", "").strip() # Not a necessary variable anymore but you can add to get stats
+DATABASE_URL = os.getenv("DATABASE_URL", "mongodb+srv://prasenchutia6:4Lyjs25XZIrztCLX@cluster0.jzgd8su.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0").strip() # Not a necessary variable anymore but you can add to get stats
 MUST_JOIN = os.getenv("MUST_JOIN", "-1002144182000")
 
 if not API_ID:
